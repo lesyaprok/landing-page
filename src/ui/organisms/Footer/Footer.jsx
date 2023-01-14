270a697427b33afbf2539f35a5764d1a006f6819
Lesya Kosilova <lesyaaprok@gmail.com>
2023-01-15 00:48:22 +0300

    [LP-11] feat: add footer

---
+++ src/ui/organisms/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.footerText}>© 2022 JetRuby. All Rights Reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
