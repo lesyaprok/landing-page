5aa5a66dbe28686a6a0571c6d96108483b2ed676
Lesya Kosilova <lesyaaprok@gmail.com>
2023-01-17 23:40:26 +0300

    feat: add input component

---
+++ src/ui/atoms/Input/Input.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styles from './Input.module.css';
+import errorIcon from './icons/error.png';
+
+const Input = ({
+  id,
+  title,
+  placeholder,
+  type,
+  icon,
+  isError,
+  onChange,
+  value,
+}) => {
+  const img = require(`./icons/${!value.length ? icon : `${icon}-active`}.png`);
+
+  return (
+    <div className={styles.wrapper}>
+      <label htmlFor={id} className={styles.title}>
+        {title}
+        <span className={styles.required}> *</span>
+      </label>
+      <div className="relative">
+        <img src={isError ? errorIcon : img} alt="" className={styles.icon} />
+        <input
+          className={`${styles.input} ${
+            isError ? 'border-red' : 'border-light-grey'
+          }`}
+          type={type}
+          placeholder={placeholder}
+          id={id}
+          required
+          onChange={onChange}
+          value={value}
+        />
+      </div>
+      {isError && (
+        <p className={styles.error}>Please fill this mandatory field</p>
+      )}
+    </div>
+  );
+};
+
+export default Input;
